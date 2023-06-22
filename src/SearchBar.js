376c30403fa2ba45b78e68a38887eaf250fb1187
nhillson <nicholas.hillson@gmail.com>
2023-06-22 17:32:41 -0500

    Arrow Notation to fulfill same role

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,24 +2,22 @@ import { useState } from "react";
 
 
 function SearchBar(props) {
-    const [state, setState] = useState({
-        name: "",
-        price: 0,
-        type: "",
-        brand: "",
-    })
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState(0);
+    const [type, setType] = useState("");
+    const [brand, setBrand] = useState("");
     return (
         <div>
             <h2>Search for an item</h2>
             <form>
                 <label for="name-field">Name: </label>
-                <input id="name-field" type="text" value={state.name}></input>
+                <input id="name-field" type="text" value={state.name} onChange={ (event) => setName(event.target.value)}></input>
                 <label for="price-field">Max Price: </label>
-                <input id="price-field" type="number" value={state.price}></input>
+                <input id="price-field" type="number" value={state.price} onChange={ (event) => setPrice(event.target.value)}></input>
                 <label for="type-field">Type: </label>
-                <input id="type-field" type="text" value={state.type}></input>
+                <input id="type-field" type="text" value={state.type} onChange={ (event) => setType(event.target.value)}></input>
                 <label for="brand-field">Brand: </label>
-                <input id="brand-field" type="text" value={state.brand}></input>
+                <input id="brand-field" type="text" value={state.brand} onChange={ (event) => setBrand(event.target.value)}></input>
                 <button type="button">Search</button>
             </form>
         </div>
