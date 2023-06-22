a65c276f62180dbc6f1b49677a99f08c37a851f9
nhillson <nicholas.hillson@gmail.com>
2023-06-22 17:27:20 -0500

    Arrow Notation

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -13,13 +13,25 @@ function SearchBar(props) {
             <h2>Search for an item</h2>
             <form>
                 <label for="name-field">Name: </label>
-                <input id="name-field" type="text" value={state.name}></input>
+                <input id="name-field" type="text" value={state.name} onChange = { (event) => setState({
+                    ...state,
+                    name: event.target.value
+                })}></input>
                 <label for="price-field">Max Price: </label>
-                <input id="price-field" type="number" value={state.price}></input>
+                <input id="price-field" type="number" value={state.price} onChange = { (event) => setState({
+                    ...state,
+                    price: event.target.value
+                })}></input>
                 <label for="type-field">Type: </label>
-                <input id="type-field" type="text" value={state.type}></input>
+                <input id="type-field" type="text" value={state.type} onChange = { (event) => setState({
+                    ...state,
+                    type: event.target.value
+                })}></input>
                 <label for="brand-field">Brand: </label>
-                <input id="brand-field" type="text" value={state.brand}></input>
+                <input id="brand-field" type="text" value={state.brand} onChange = { (event) => setState({
+                    ...state,
+                    brand: event.target.value
+                })}></input>
                 <button type="button">Search</button>
             </form>
         </div>
