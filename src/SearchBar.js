750838774a586bd7dc9a8e941b51577d3a046eb3
nhillson <nicholas.hillson@gmail.com>
2023-06-22 17:47:19 -0500

    prettified input fields

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -11,13 +11,33 @@ function SearchBar(props) {
             <h2>Search for an item</h2>
             <form>
                 <label for="name-field">Name: </label>
-                <input id="name-field" type="text" value={name} onChange={ (event) => setName(event.target.value)}></input>
+                <input 
+                    id="name-field" 
+                    type="text" 
+                    value={name} 
+                    onChange={ (event) => setName(event.target.value)}
+                />
                 <label for="price-field">Max Price: </label>
-                <input id="price-field" type="number" value={price} onChange={ (event) => setPrice(event.target.value)}></input>
+                <input 
+                    id="price-field" 
+                    type="number" 
+                    value={price} 
+                    onChange={ (event) => setPrice(event.target.value)}
+                />
                 <label for="type-field">Type: </label>
-                <input id="type-field" type="text" value={type} onChange={ (event) => setType(event.target.value)}></input>
+                <input 
+                    id="type-field" 
+                    type="text" 
+                    value={type} 
+                    onChange={ (event) => setType(event.target.value)}
+                />
                 <label for="brand-field">Brand: </label>
-                <input id="brand-field" type="text" value={brand} onChange={ (event) => setBrand(event.target.value)}></input>
+                <input 
+                    id="brand-field" 
+                    type="text" 
+                    value={brand} 
+                    onChange={ (event) => setBrand(event.target.value)}
+                />
                 <button type="button">Search</button>
             </form>
         </div>
